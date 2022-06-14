b32879e006c3d5dc0529d2a82eb4fc38e64f8283
aderyagin <aderyagin@ADERYAGIN-PC>
2022-06-14 16:39:00 +0300

    add ArduinoUploader (nuget) and USB

---
+++ ArduinoLedController/ArduinoUsbPort.cs
@@ -0,0 +1,40 @@
+﻿using System.IO.Ports;
+
+namespace ArduinoLedController
+{
+    public class ArduinoUsbPort
+    {
+        SerialPort currentPort;
+        public string GetCorrectArduinoUsbPort()
+        {
+            string result = null;
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                this.currentPort = new SerialPort(port, 9600);
+                if (IsCorrectArduinoUsbPort())
+                {
+                    result = port;
+                }
+            }
+            return result;
+        }
+        public bool IsCorrectArduinoUsbPort()
+        {
+            currentPort.Open();
+            System.Threading.Thread.Sleep(1000);
+            string returnMessage = currentPort.ReadLine();
+            currentPort.Close();
+            // in arduino sketch should be Serial.println("Info from Arduino") inside  void loop()
+            if (returnMessage.Contains("Info from Arduino"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- ArduinoLedController/MainWindow.xaml.cs
+++ ArduinoLedController/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
-using System.Windows.Media;
 using System.IO.Ports; // for COM Ports
+using ArduinoUploader;
+using ArduinoUploader.Hardware;
 
 namespace ArduinoLedController
 {
@@ -9,35 +10,48 @@ namespace ArduinoLedController
     /// </summary>
     public partial class MainWindow : Window
     {
-        static string ComPort = "COM6";
         static int speed = 9600; //between 300 and 2'000'000
-
-        SerialPort mySerialPort = new SerialPort(ComPort, speed);
+        static string pathHex = @"C:\Users\aderyagin\Documents\Arduino\sketch_jun14a\sketch_jun14a.ino.with_bootloader.standard.hex"; //path to the hex file
+        //private delegate void updateDelegate(string txt);
         private byte btnstate = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+            ArduinoUsbPort arduinoUsbPort = new ArduinoUsbPort();
+            string usbPort = arduinoUsbPort.GetCorrectArduinoUsbPort();
+            var uploader = new ArduinoSketchUploader(new ArduinoSketchUploaderOptions()
+            {
+                FileName = pathHex,
+                PortName = usbPort,
+                ArduinoModel = ArduinoModel.UnoR3 //Model
+            }
+            );
+            uploader.UploadSketch();
+
+            
+
         }
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
+            ArduinoUsbPort arduinoUsbPort = new ArduinoUsbPort();
+            string usbPort = arduinoUsbPort.GetCorrectArduinoUsbPort();
+            SerialPort serialPort = new SerialPort(usbPort, speed);
             if (btnstate == 0)
             {
-                mySerialPort.Open();
-                mySerialPort.Write("1");
-                mySerialPort.Close();
+                serialPort.Open();
+                serialPort.Write("1");
+                serialPort.Close();
                 btnstate = 1;
-                btn1.Background = Brushes.Green;
                 btn1.Content = "Red Led ON";
             }
             else
             {
-                mySerialPort.Open();
-                mySerialPort.Write("2");
-                mySerialPort.Close();
+                serialPort.Open();
+                serialPort.Write("2");
+                serialPort.Close();
                 btnstate = 0;
-                btn1.Background = Brushes.Red;
                 btn1.Content = "Red Led OFF";
             }
         }
